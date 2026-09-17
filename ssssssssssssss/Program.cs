2c313410d6b859c55867b07d87ce6f639a07a8ad
SzilardVince <szilard.vince.2024d@telekimezotur.hu>
2026-09-17 12:43:58 +0200

    feladat 4.

--- ssssssssssssss/Program.cs
+++ ssssssssssssss/Program.cs
@@ -3,9 +3,19 @@ int megtettav = 14;
 bool eszakai = true;
 double alapdij = 1100;
 double kmdij = 440;
+double osz2=0;
 double eszakai_potlek = 1.20;
 double osz1 = alapdij+(kmdij*megtettav);
 if (eszakai)
 {
-    osz1 = osz1 * eszakai_potlek;
+    osz2 = osz1 * eszakai_potlek;
 }
+else
+{
+    osz2 = osz1;
+}
+Console.WriteLine($"utas neve: {nev}");
+Console.WriteLine($"megtett távolsag: {megtettav} km");
+Console.WriteLine($"alapertek: {osz1} Ft");
+Console.WriteLine($"fizetendo osszeg: {osz2} Ft");
+Console.WriteLine($"státusz: {(eszakai_potlek-1)*100} %");
