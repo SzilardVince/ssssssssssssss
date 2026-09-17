24c3afd876105399569e18120722b5aa6ea2becd
SzilardVince <szilard.vince.2024d@telekimezotur.hu>
2026-09-17 12:36:03 +0200

    feladat 2.

--- ssssssssssssss/Program.cs
+++ ssssssssssssss/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿string nev = "Márton Károly";
+int megtettav = 14;
+bool eszakai = true;
+double alapdij = 1100;
+double kmdij = 440;
+double eszakai_potlek = 1.20;
+double osz1 = alapdij+(kmdij*megtettav);
